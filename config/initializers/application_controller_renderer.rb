4ce8af641b1fe362dfbe66f2b53a4f24aafb9b3a
shuri <syurimoririn@gmail.com>
2020-02-17 12:33:33 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '842a00204a2cd630d4db3395c18ff90b98fed8efd1aa46e5146cb659a05ce70a48cd06b037c1eceffbd00243fa8b04540473a2ef4e4582bcff40a5a1c1079152'
